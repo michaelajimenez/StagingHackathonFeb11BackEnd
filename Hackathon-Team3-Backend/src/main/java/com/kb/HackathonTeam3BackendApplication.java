20f7ad3f718c3d89b5103288ba7d5c962274b970
Albert Washington <Albert.LWash@gmail.com>
2021-02-11 10:14:25 -0600

    Spring boot application created.

---
+++ Hackathon-Team3-Backend/src/main/java/com/kb/HackathonTeam3BackendApplication.java
@@ -0,0 +1,13 @@
+package com.kb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonTeam3BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonTeam3BackendApplication.class, args);
+	}
+
+}
